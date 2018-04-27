3b9e0f714c6dbcdc629eef2ac535de60242abe5e
Alfredo <dpd.12qwaszx>
2018-04-27 17:39:43 -0300

    first commit

---
+++ app/src/main/java/br/com/android/ktempconverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.android.ktempconverter
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+    }
+}

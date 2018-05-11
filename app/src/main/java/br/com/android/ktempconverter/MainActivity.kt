e771ad5f4f9c1fb721695dfe9d604946d8e25653
Alfredo <dpd.12qwaszx>
2018-05-11 16:05:52 -0300

    Configurando MainActivity em AndroidManifest.

--- app/src/main/java/br/com/android/ktempconverter/MainActivity.kt
+++ app/src/main/java/br/com/android/ktempconverter/MainActivity.kt
@@ -1,12 +1,11 @@
 package br.com.android.ktempconverter
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
